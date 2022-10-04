84ac7bf514fc1eca4e6e6e1c6e9f07635da6ad04
Pacinho19 <pacio170@interia.pl>
2022-10-04 14:45:56 +0200

    Changing bless to soul rate (2 -> 1.5)

--- src/main/java/pl/pacinho/muonlinewebtrader/model/dto/CurrencyRateDto.java
+++ src/main/java/pl/pacinho/muonlinewebtrader/model/dto/CurrencyRateDto.java
@@ -7,6 +7,6 @@ import java.math.BigDecimal;
 @Getter
 public class CurrencyRateDto {
 
-    private final BigDecimal blessToSoulRate = BigDecimal.valueOf(2L);
+    private final BigDecimal blessToSoulRate = BigDecimal.valueOf(1.5);
     private final BigDecimal soulToBlessRate = BigDecimal.valueOf(0.25);
 }
